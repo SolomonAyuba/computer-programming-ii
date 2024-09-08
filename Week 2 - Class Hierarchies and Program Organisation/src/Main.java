e5c5ec0a1ea7b5f631dfa84839cf3d53c5a03ff1
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-09-08 15:51:04 +0100

    Week 2: Class Hierarchies and Program Organisation

---
+++ Week 2 - Class Hierarchies and Program Organisation/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // WEEK 2: Class Hierarchies and Program Organisation  |	Sun 8th September, 2024
+
+        // For spacing between codes
+        String space = "---------";
+        System.out.println(space + "WEEK 2: Class Hierarchies and Program Organisation  |\tSun 8th September, 2024" + space);
+
+
+        // Organising Class Hierarchies
+
+        // File: MyClass.java
+        package mypackage;
+
+        public class MyClass {
+            // Public method accessible to other classes within the same package
+            public void myMethod() {
+                // some Code here
+            }
+        }
+    }
+}
---
+++ Week 2 - Class Hierarchies and Program Organisation/src/namespaces.java
@@ -0,0 +1,12 @@
+public class namespaces {
+    // This is intended for C++ - you can ignore.
+
+    // File: MyClass.h
+    namespace mynamespace {
+        class MyClass{
+            public:
+                // Public method
+                 void myMethod();
+        };
+    }
+}
